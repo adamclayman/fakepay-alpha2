c3da32a2f6c210358c3afa82f90325dffa499d05
Adam <adam@clayman.org>
2018-07-29 09:46:06 -0400

    Modifies scope of routes to /api/alpha2/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :subscriptions
-  resources :plans
-  resources :customers
+  scope '/api' do
+    scope '/alpha2' do
+      resources :subscriptions
+      resources :plans
+      resources :customers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
