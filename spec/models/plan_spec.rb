4f80a2841fa76a3485fc604ee566fb2805135190
Adam <adam@clayman.org>
2018-07-30 11:28:45 -0400

    Secures plan price from decimalized price values

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Plan, type: :model do
     @plan = FactoryBot.build(:valid_plan, price: "")
     expect(@plan).to_not be_valid
   end
+  it 'should not validate plans with a decimalized price' do
+    @plan = FactoryBot.build(:valid_plan, price: "99.00")
+    expect(@plan).to_not be_valid
+  end
   it 'should not validate plans without a period' do
     @plan = FactoryBot.build(:valid_plan, period: "")
     expect(@plan).to_not be_valid
